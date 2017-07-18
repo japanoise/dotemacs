fd5191671069ea31d6dbc52c2fbb01742b912c44
japanoise <japanoise@tfwno.gf>
2017-07-18 09:40:55 +0100

    Add format hooks to go-mode

--- init.el
+++ init.el
@@ -91,10 +91,24 @@
 ;; Go mode. Dependencies:
 ;; - github.com/nsf/gocode
 ;; - github.com/godoctor/godoctor
+;; - golang.org/x/tools/cmd/goimports
 (use-package go-mode
   :config (use-package godoctor)
   :bind(("C-c C-r" . go-remove-unused-imports)))
 (use-package company-go)
+(defun my-go-mode-hook ()
+  ; Use goimports instead of go-fmt
+  (setq gofmt-command "goimports")
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
 
 ;; Web mode
 (use-package web-mode
