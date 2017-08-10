e1684589a787a12f1b0d68ef9776adc45d0cc629
japanoise <japanoise@tfwno.gf>
2017-08-10 12:41:38 +0100

    Add autoformatting to elisp

--- chameleon/chameleon-prog.el
+++ chameleon/chameleon-prog.el
@@ -21,6 +21,14 @@
 
 ;;; Code:
 
+;; Elisp
+(use-package srefactor)
+(require 'srefactor-lisp)
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook 'srefactor-lisp-format-buffer)
+            (setq indent-tabs-mode nil)))
+
 ;; Go mode. Dependencies:
 ;; - github.com/nsf/gocode
 ;; - github.com/godoctor/godoctor
@@ -28,30 +36,34 @@
 ;; - github.com/rogpeppe/godef
 ;; - golang.org/x/tools/cmd/gorename
 (use-package go-mode
-  :config (use-package godoctor)
-  :bind(("C-c C-r" . go-remove-unused-imports)))
+  :config (use-package godoctor):bind
+  (("C-c C-r" . go-remove-unused-imports)))
 (use-package company-go)
 (defun my-go-mode-hook ()
   "Hook for go mode.  Use goimports, godef, setup compile command."
-  ; Use goimports instead of go-fmt
+  ;; Use goimports instead of go-fmt
   (setq gofmt-command "goimports")
-  ; Call Gofmt before saving
+  ;; Call Gofmt before saving
   (add-hook 'before-save-hook 'gofmt-before-save)
-  ; Customize compile command to run go build
+  ;; Customize compile command to run go build
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
            "go build -v && go test -v && go vet"))
-  ; Godef jump key binding
-  (local-set-key (kbd "M-.") 'godef-jump)
-  (local-set-key (kbd "M-*") 'pop-tag-mark)
+  ;; Godef jump key binding
+  (local-set-key (kbd "M-.")
+                 'godef-jump)
+  (local-set-key (kbd "M-*")
+                 'pop-tag-mark)
   ;; More known behaviour from IntelliJ IDEA
-  (local-set-key (kbd "<C-down-mouse-1>") 'mouse-set-point)
-  (local-set-key (kbd "<C-mouse-1>") 'godef-jump)
-  (set (make-local-variable 'company-backends) '(company-go)))
+  (local-set-key (kbd "<C-down-mouse-1>")
+                 'mouse-set-point)
+  (local-set-key (kbd "<C-mouse-1>")
+                 'godef-jump)
+  (set (make-local-variable 'company-backends)
+       '(company-go)))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 (use-package go-eldoc
-  :init
-  (add-hook 'go-mode-hook 'go-eldoc-setup))
+  :init (add-hook 'go-mode-hook 'go-eldoc-setup))
 (use-package go-rename
   :bind (("<S-f6>" . go-rename)))
 
@@ -62,21 +74,26 @@
 (defun my-web-mode-hook ()
   "Hook for web-mode with smartparens."
   (setq web-mode-enable-auto-pairing nil))
-(add-hook 'web-mode-hook  'my-web-mode-hook)
+(add-hook 'web-mode-hook 'my-web-mode-hook)
 (defun sp-web-mode-is-code-context (id action context)
   "Determines whether we're in code context in web-mode.  Takes args ID ACTION CONTEXT."
   (and (eq action 'insert)
-       (not (or (get-text-property (point) 'part-side)
-                (get-text-property (point) 'block-side)))))
-(sp-local-pair 'web-mode "<" nil :when '(sp-web-mode-is-code-context))
+       (not (or (get-text-property (point)
+                                   'part-side)
+                (get-text-property (point)
+                                   'block-side)))))
+(sp-local-pair 'web-mode
+               "<"
+               nil
+               :when '(sp-web-mode-is-code-context))
 
 ;; Markdown mode
 (use-package markdown-mode
   :ensure t
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
+  :commands (markdown-mode gfm-mode):mode
+  (("README\\.md\\'" . gfm-mode)
+   ("\\.md\\'" . markdown-mode)
+   ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
 
 ;; Haskell mode.
@@ -86,7 +103,8 @@
             (set (make-local-variable 'company-backends)
                  (append '((company-capf company-dabbrev-code))
                          company-backends))))
-(setq haskell-completions-complete-operators nil)
+(setq haskell-completions-complete-operators
+      nil)
 
 (provide 'chameleon-prog)
 ;;; chameleon-prog.el ends here
--- init.el
+++ init.el
@@ -6,9 +6,12 @@
 ;; ## Set up package lists & use-package
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -30,15 +33,17 @@
   (define-key chameleon-prefix-map (kbd "r") 'replace-regexp)
   (define-key chameleon-prefix-map (kbd "q r") 'query-replace-regexp)
   (define-key chameleon-prefix-map (kbd "q s") 'query-replace))
-(global-set-key (kbd "<f5>") 'chameleon-prefix-map)
+(global-set-key (kbd "<f5>")
+                'chameleon-prefix-map)
 
 (load-file "~/.emacs.d/chameleon/chameleon-packages.el")
 (load-file "~/.emacs.d/chameleon/chameleon-prog.el")
 
 ;; ## Major mode hooks
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(add-hook 'org-mode-hook
+          (lambda ()
+            (org-bullets-mode 1)))
 (add-hook 'prog-mode-hook #'smartparens-mode)
-(add-hook 'emacs-lisp-mode-hook (lambda () (setq indent-tabs-mode nil)))
 
 ;; ## Minor Modes
 (require 'paren)
@@ -67,23 +72,27 @@
 
 (unless my/local-theme
   (load-file "~/.emacs.d/chameleon/xemacs-chameleon-theme.el")
-  (use-package doom-themes :config
-    (setq doom-vibrant-brighter-comments t)
-    (setq doom-vibrant-brighter-modeline t)
-    (setq doom-vibrant-comment-bg t))
-  (defvar chameleon/themes '(doom-vibrant xemacs-chameleon) "Themes to rotate through.")
-  (defun chameleon/rotate-themes () "Switch to the next theme in chameleon/themes."
-         (interactive)
-         (require 'dash)
-         (mapc (lambda (theme)
-                 (when (member theme chameleon/themes)
-                   (disable-theme theme)))
-               custom-enabled-themes)
-         (smart-mode-line-enable)
-         (load-theme (car chameleon/themes) t)
-         (setq chameleon/themes (-rotate (- (length chameleon/themes) 1) chameleon/themes))
-         (redraw-display))
-  (global-set-key (kbd "C-c C-t") 'chameleon/rotate-themes))
+  (use-package doom-themes
+    :config (setq doom-vibrant-brighter-comments t)(setq doom-vibrant-brighter-modeline t)(setq doom-vibrant-comment-bg t))
+  (defvar chameleon/themes '(doom-vibrant xemacs-chameleon)
+    "Themes to rotate through.")
+  (defun chameleon/rotate-themes ()
+    "Switch to the next theme in chameleon/themes."
+    (interactive)
+    (require 'dash)
+    (mapc (lambda (theme)
+            (when (member theme chameleon/themes)
+              (disable-theme theme)))
+          custom-enabled-themes)
+    (smart-mode-line-enable)
+    (load-theme (car chameleon/themes)
+                t)
+    (setq chameleon/themes (-rotate (- (length chameleon/themes)
+                                       1)
+                                    chameleon/themes))
+    (redraw-display))
+  (global-set-key (kbd "C-c C-t")
+                  'chameleon/rotate-themes))
 
 ;; http://pages.sachachua.com/.emacs.d/Sacha.html
 ;; Save lots of history
@@ -93,10 +102,7 @@
 (setq history-length t)
 (setq history-delete-duplicates t)
 (setq savehist-save-minibuffer-history 1)
-(setq savehist-additional-variables
-      '(kill-ring
-        search-ring
-        regexp-search-ring))
+(setq savehist-additional-variables '(kill-ring search-ring regexp-search-ring))
 
 ;; Enabled commands
 (put 'downcase-region 'disabled nil)
@@ -104,7 +110,8 @@
 
 ;; ## Custom-set
 (load-file "~/.emacs.d/chameleon/chameleon-custom.el") ;; Boot this to another file.
-(if my/local-theme (enable-theme my/local-theme)
+(if my/local-theme
+    (enable-theme my/local-theme)
   (enable-theme 'xemacs-chameleon))
 (use-package smart-mode-line) ;; Make the modeline suck less; this needs to come after customise
 (setq sml/theme 'respectful)
