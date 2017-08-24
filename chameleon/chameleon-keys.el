42cf0911f8abe0d6f342ce7c7809fa23925358c2
japanoise <japanoise@tfwno.gf>
2017-08-24 10:24:21 +0100

    Dired mode bindings + use windmove

--- chameleon/chameleon-keys.el
+++ chameleon/chameleon-keys.el
@@ -21,32 +21,49 @@
 
 ;;; Code:
 
-(global-set-key (kbd "M-]") 'flycheck-next-error)
-(global-set-key (kbd "M-[") 'flycheck-previous-error)
+(global-set-key (kbd "M-]")
+                'flycheck-next-error)
+(global-set-key (kbd "M-[")
+                'flycheck-previous-error)
 
-(global-set-key (kbd "C-z") 'scroll-down-command)
-(global-set-key (kbd "C-M-z") 'scroll-other-window-down) ;; Nice command from uemacs
+(global-set-key (kbd "C-z")
+                'scroll-down-command)
+(global-set-key (kbd "C-M-z")
+                'scroll-other-window-down) ;; Nice command from uemacs
 
-(global-set-key (if (string-equal system-type "windows-nt") (kbd "<apps>") (kbd "<menu>")) 'helm-M-x)
+(global-set-key (if (string-equal system-type "windows-nt")
+                    (kbd "<apps>")
+                  (kbd "<menu>"))
+                'helm-M-x)
 
-(global-set-key [S-mouse-2] 'browse-url-at-mouse)
+(global-set-key [S-mouse-2]
+                'browse-url-at-mouse)
 
 ;; Use f6 as uarg, free up C-u for kill line backwards.
-(global-set-key [f6] 'universal-argument)
+(global-set-key [f6]
+                'universal-argument)
 (define-key universal-argument-map [f6] 'universal-argument-more)
-(define-key universal-argument-map "\C-u" nil)
-(defun kill-line-backwards () "Kill line backwards." (interactive) (kill-line 0))
-(global-set-key (kbd "C-u") 'kill-line-backwards)
+(define-key universal-argument-map "\C-u"
+  nil)
+(defun kill-line-backwards ()
+  "Kill line backwards."
+  (interactive)
+  (kill-line 0))
+(global-set-key (kbd "C-u")
+                'kill-line-backwards)
 
 ;; Nice commands from sandy
-(global-set-key (kbd "<S-home>") 'beginning-of-buffer)
-(global-set-key (kbd "<S-end>") 'end-of-buffer)
+(global-set-key (kbd "<S-home>")
+                'beginning-of-buffer)
+(global-set-key (kbd "<S-end>")
+                'end-of-buffer)
 (defun filter-replace-region ()
   "Filter the region through a shell command, replacing it."
   (interactive)
   (let ((current-prefix-arg '(0)))
     (call-interactively #'shell-command-on-region)))
-(global-set-key (kbd "C-\\") 'filter-replace-region)
+(global-set-key (kbd "C-\\")
+                'filter-replace-region)
 
 ;; http://whattheemacsd.com/file-defuns.el-01.html
 ;; Replace a useless binding (find-file-read-only) with a useful one.
@@ -55,18 +72,31 @@
   (interactive)
   (let ((name (buffer-name))
         (filename (buffer-file-name)))
-    (if (not (and filename (file-exists-p filename)))
-        (error "Buffer '%s' is not visiting a file!" name)
+    (if (not (and filename
+                  (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!"
+               name)
       (let ((new-name (read-file-name "New name: " filename)))
         (if (get-buffer new-name)
-            (error "A buffer named '%s' already exists!" new-name)
+            (error "A buffer named '%s' already exists!"
+                   new-name)
           (rename-file filename new-name 1)
           (rename-buffer new-name)
           (set-visited-file-name new-name)
           (set-buffer-modified-p nil)
           (message "File '%s' successfully renamed to '%s'"
-                   name (file-name-nondirectory new-name)))))))
-(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+                   name
+                   (file-name-nondirectory new-name)))))))
+(global-set-key (kbd "C-x C-r")
+                'rename-current-buffer-file)
+
+;; Muscle memory from helm
+(require 'dired)
+(define-key dired-mode-map (kbd "C-l") 'dired-up-directory)
+(define-key dired-mode-map (kbd "TAB") 'dired-find-file)
+
+;; Better use for S-<arrow> - fast window switching
+(windmove-default-keybindings)
 
 (provide 'chameleon-keys)
 ;;; chameleon-keys.el ends here
